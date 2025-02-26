65bd3921201e483e6edef66ffddc5e760fab173b
Security Savage <scrtsvg@gmail.com>
2025-02-26 16:09:15 -0700

    set up player, added commands to main/constants

--- main.py
+++ main.py
@@ -2,11 +2,13 @@
 
 import pygame
 from constants import * # load pygame library and constants module
+from player import Player # load player module
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # screen object
     clock = pygame.time.Clock() # clock object
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0 # establish counter
     
     while True:
@@ -15,6 +17,7 @@ def main():
                 return
         
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         # limit the framerate to 60fps
