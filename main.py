92769bd2166d7d1a87f2f00e510095aee3555417
Security Savage <scrtsvg@gmail.com>
2025-02-26 15:45:58 -0700

    set up clock in main and added circleshape.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
 import pygame
-from constants import *
+from constants import * # load pygame library and constants module
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # screen object
+    clock = pygame.time.Clock() # clock object
+    dt = 0 # establish counter
     
     while True:
         for event in pygame.event.get():
@@ -14,6 +16,9 @@ def main():
         
         screen.fill("black")
         pygame.display.flip()
+
+        # limit the framerate to 60fps
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
